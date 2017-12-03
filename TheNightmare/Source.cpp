b9d4a827a31b5579294af47b271ae54a0b3314dd
ahmibr <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2017-12-03 18:44:52 +0200

    Optimize model loading performance

--- TheNightmare/Source.cpp
+++ TheNightmare/Source.cpp
@@ -5,6 +5,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// #include <learnopengl/filesystem.h>
 #include <learnopengl/shader_m.h>
 #include <learnopengl/camera.h>
 #include <learnopengl/model.h>
